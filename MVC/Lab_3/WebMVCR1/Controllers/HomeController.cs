551c32be0a0166bb147b42e4335864db095532e0
Denis-Mosunov <dnsmosunov@gmail.com>
2025-03-14 21:36:01 +0300

    Add files via upload

---
+++ MVC/Lab_3/WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using WebMVCR1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Rewrite;
+using System.Text;
+
+namespace WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            ViewData["Mes"] = "хорошего настроения";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult InputData()
+        {
+            return View();
+        }
+    }
+}
