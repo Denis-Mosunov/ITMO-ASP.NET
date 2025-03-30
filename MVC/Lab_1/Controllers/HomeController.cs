fff4586e8546751e4a4c644a83fcea8071fd1d38
Denis-Mosunov <dnsmosunov@gmail.com>
2025-03-30 20:45:23 +0300

    Add files via upload

---
+++ MVC/Lab_1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string hel)
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+
+            // Добавляем параметр к приветствию, если он был передан
+            if (!string.IsNullOrEmpty(hel))
+            {
+                Greeting += ", " + hel;
+            }
+
+            return Greeting;
+        }
+    }
+}
---
+++ MVC/Lab_1/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем сервисы, необходимые для MVC
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Настраиваем конвейер обработки HTTP-запросов
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Настраиваем маршрутизацию
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{hel?}");
+
+app.Run(); 
